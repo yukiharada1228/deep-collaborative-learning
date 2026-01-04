132d2f7556909a3e45a9c563a70d7775d0d5ab23
yukiharada1228 <yukiharada1228@gmail.com>
2026-01-04 10:14:14 +0900

    chore: Remove LinearGate and CorrectGate from default `--gates` argument.

--- examples/CIFAR100/dcl_search.py
+++ examples/CIFAR100/dcl_search.py
@@ -27,7 +27,7 @@ parser.add_argument(
 )
 parser.add_argument(
     "--gates",
-    default=["ThroughGate", "CutoffGate", "LinearGate", "CorrectGate"],
+    default=["ThroughGate", "CutoffGate"],
     nargs="*",
     type=str,
 )
